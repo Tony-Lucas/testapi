153e6350ba7c9eb577fba5a2207b88fc2ccd6d6a
Tony Silva <tonysilva@MacBook-Air-de-Tony.local>
2021-05-07 10:58:16 -0300

    asdasdas

--- src/server.js
+++ src/server.js
@@ -5,26 +5,28 @@ const multer = require("multer");
 require("dotenv").config();
 require("./config/relations");
 const storage = multer.diskStorage({
-    destination: (req,file,cb) => {
-        cb(null,"./uploads/")
+    destination: (req, file, cb) => {
+        cb(null, "./uploads/")
     },
-    filename: (req,file,cb) => {
-        cb(null,Date.now() + file.originalname)
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + file.originalname)
     }
 })
-const upload = multer({storage: storage}).single("img")
+const upload = multer({ storage: storage }).single("img")
 
 app.use(express.static("uploads"))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.post("/",(req,res) => {
+app.post("/", (req, res) => {
     upload(req, res, function (err) {
         if (err instanceof multer.MulterError) {
-          res.json({error:"Ocorreu um erro"})
+            res.json({ error: "Ocorreu um erro" })
         } else if (err) {
-            res.json({error:"Ocorreu um erro desconhecido"})
+            res.json({ error: "Ocorreu um erro desconhecido" })
         }
+
+    })
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
